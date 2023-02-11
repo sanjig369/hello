a43ae6531dbd838ed428f02a7e4eca520fe73535
sanjig369 <117338042+sanjig369@users.noreply.github.com>
2023-02-11 11:46:38 +0800

    test
    111

---
+++ Test3.java
@@ -0,0 +1,16 @@
+package com.fujianglong.test;
+public class Test3 {
+    public static void main(String[] args) {
+        int b[][]={{1,1,1},{2,2,2,},{3,3}};
+        int sum=0;
+        for (int i = 0; i <b.length ; i++) {
+            for (int j=0;j<b[i].length;j++){
+                sum=b[i][j] ;
+            }
+        }
+    }
+    System.out.println("s")
+    }
+
+
+
